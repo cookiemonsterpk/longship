7f303cdfdd10d8938fc801d359424b317b80e7b5
Scott Vinkle <svinkle@gmail.com>
2013-09-21 17:18:39 -0400

    first iteration of 'our process' section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,13 +6,7 @@ $('body').on('click', '.js-data-list-button', function () {
 $('body').on('click', '.js-data-list-button--our-services', function () {	
 	$('.js-data-list-button--our-services, .js-data-list-content--our-services').removeClass('active');
 	$(this).toggleClass('active');
-
-	console.log($(this).parent('dt').next('.js-data-list-content--our-services').hasClass('active'));
-	if ($(this).parent('dt').next('.js-data-list-content--our-services').hasClass('active')) {
-		$(this).parent('dt').next('.js-data-list-content--our-services').removeClass('active');
-	} else {
-		$(this).parent('dt').next('.js-data-list-content--our-services').addClass('active');
-	}
+	$(this).parent('dt').next('.js-data-list-content--our-services').addClass('active');
 });
 
 $('body').on('click', '.js-data-list-content--our-services-close', function () {
