7640d7aac5a984686903fad616eafef0455993d5
Scott Vinkle <svinkle@gmail.com>
2013-09-21 12:26:11 -0400

    updated 'our services' section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,19 @@ $('body').on('click', '.js-data-list-button', function () {
 	$(this).toggleClass('active');
 	$(this).parent('dt').next('.js-data-list-content').toggleClass('active');
 });
+
+$('body').on('click', '.js-data-list-button--our-services', function () {	
+	$('.js-data-list-button--our-services, .js-data-list-content--our-services').removeClass('active');
+	$(this).toggleClass('active');
+
+	console.log($(this).parent('dt').next('.js-data-list-content--our-services').hasClass('active'));
+	if ($(this).parent('dt').next('.js-data-list-content--our-services').hasClass('active')) {
+		$(this).parent('dt').next('.js-data-list-content--our-services').removeClass('active');
+	} else {
+		$(this).parent('dt').next('.js-data-list-content--our-services').addClass('active');
+	}
+});
+
+$('body').on('click', '.js-data-list-content--our-services-close', function () {
+	$('.js-data-list-button--our-services, .js-data-list-content--our-services').removeClass('active');
+});
